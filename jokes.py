795b9344ab1e385dfe7627dda7e0e0d48476d8b6
uhcltech <uhcltech@pclab.uhcl.edu>
2018-03-23 14:12:03 -0500

    Alexa asks user question

--- jokes.py
+++ jokes.py
@@ -4,6 +4,12 @@ from flask_ask import Ask, statement, question
 app = Flask (__name__)
 ask = Ask(app, '/')
 
+@ask.launch
+def launched():
+    return question('Welcome to jokes! What kind of joke do you want to hear, '\
+    'a one liner or a knock knock joke?')\
+    .reprompt('Sorry I missed that. Would you like to hear a one liner or a knock knock joke?')
+
 @ask.intent('OneLiner')
 def one_liner():
     return statement('Warning, keyboard not found. Press Enter to continue.')
